0edf8920238e5e89155a9939865adfd6f3a22c5f
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-18 15:24:27 +0000

    ♻️ Make AnimatedObject extend StaticObject

--- src/SimulationObject.ts
+++ src/SimulationObject.ts
@@ -1,10 +1,9 @@
 import * as THREE from "three";
 
-export interface AnimatedObject {
-    mesh: THREE.Mesh;
-    update(): void;
-}
-
 export interface StaticObject {
     mesh: THREE.Mesh;
+}
+
+export interface AnimatedObject extends StaticObject {
+    update(): void;
 }
