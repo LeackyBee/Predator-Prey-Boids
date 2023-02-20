be64f537f1b4c44502f3b54174005c82d8a6d633
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-20 09:49:52 +0000

    Add option to draw gridlines on floor

--- src/SimulationObject.ts
+++ src/SimulationObject.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 
 export interface StaticObject {
-    mesh: THREE.Mesh;
+    mesh: THREE.Object3D<THREE.Event>;
 }
 
 export interface AnimatedObject extends StaticObject {
--- src/objects/Floor.ts
+++ src/objects/Floor.ts
@@ -1,15 +1,32 @@
 import * as THREE from "three";
 import { StaticObject } from "../SimulationObject";
 
-export class Floor implements StaticObject {
-    mesh: THREE.Mesh;
+export interface FloorOptions {
+    withGrid?: boolean;
+}
 
-    constructor() {
-        const geometry = new THREE.PlaneGeometry(1000, 1000);
+export class Floor implements StaticObject {
+    mesh: THREE.Object3D<THREE.Event>;
+
+    private static readonly SIZE = 1000;
+
+    constructor(options?: FloorOptions) {
+        const geometry = new THREE.PlaneGeometry(Floor.SIZE, Floor.SIZE);
         const material = new THREE.MeshBasicMaterial({ color: 0xd4d4d8 });
-        this.mesh = new THREE.Mesh(geometry, material);
+        const floorMesh = new THREE.Mesh(geometry, material);
 
         // make the plane horizontal
-        this.mesh.rotateX(-Math.PI / 2);
+        floorMesh.rotateX(-Math.PI / 2);
+        // so that the floor doesn't "disappear" when camera is rotated to exactly horizontal
+        floorMesh.position.setY(-1);
+
+        if (options?.withGrid ?? true) {
+            const gridHelper = new THREE.GridHelper(Floor.SIZE, 20);
+            gridHelper.add(floorMesh);
+
+            this.mesh = gridHelper;
+        } else {
+            this.mesh = floorMesh;
+        }
     }
 }
