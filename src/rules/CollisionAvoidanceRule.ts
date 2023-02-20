79013b24e70d6b2bbfaa2551d40e63b912cd074d
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-20 14:50:36 +0000

    Remove distance threshold from collision rule

--- src/rules/CollisionAvoidanceRule.ts
+++ src/rules/CollisionAvoidanceRule.ts
@@ -3,16 +3,11 @@ import { Boid } from "../objects/Boid";
 import * as THREE from "three";
 
 export class CollisionAvoidanceRule extends Rule {
-    private static readonly DISTANCE_THRESHOLD = 4;
-
     calculateVector(thisBoid: Boid, args: RuleArguments): THREE.Vector3 {
         const collisionAvoidVector = new THREE.Vector3();
 
         for (const neighbour of args.neighbours) {
             const dist = thisBoid.position.distanceTo(neighbour.position);
-            if (dist > CollisionAvoidanceRule.DISTANCE_THRESHOLD) {
-                continue;
-            }
 
             const avoidanceMagnitude = Math.exp(-dist);
 
